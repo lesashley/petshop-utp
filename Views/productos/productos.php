e5e8505b2181336c3ea7c9619c41128c415771ff
Sergio Alexander Huayllas Tirado <aogiri98a@gmail.com>
2022-10-24 21:58:22 -0500

    MAYUSCULA

--- Views/productos/productos.php
+++ Views/productos/productos.php
@@ -8,7 +8,7 @@
         <div>
             <h1><?= $data['page_title'] ?>
             <?php if($_SESSION['permisosMod']['w']){ ?>
-                <button class="btn btn-primary" type="button" onclick="openModal();" ><i class="fas fa-plus-circle"></i> Nuevo</button>
+                <button class="btn btn-primary" type="button" onclick="openModal();" ><i class="fas fa-plus-circle"></i> Nuevos</button>
             <?php } ?>
             </h1>
         </div>
